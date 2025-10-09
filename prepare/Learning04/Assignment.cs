8205b49bb18ee9c7e984211b489e7340e4571387
Dallas2099 <32474910+Dallas2099@users.noreply.github.com>
2025-10-08 17:57:01 -0800

    Inheritance Learning Activity

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,21 @@
+public class Assignment
+{
+    private readonly string _studentName;
+    private readonly string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
